8fe72dd41d4348431e1d0c0cac5a89e61a80b715
Bhum1691 <phurinat1691@gmail.com>
2022-07-07 10:32:28 +0700

    Add File Exercise4

---
+++ Exercise4_Phurinat_M.py
@@ -0,0 +1,10 @@
+Foundation_English_Score = 80
+General_Business_Score = 85
+Introduction_to_Computer_Systems_Score = 79
+Computer_Programming_Score = 91
+
+print("--- Your Score ---")
+print("Foundation English :",Foundation_English_Score)
+print("General Business :",General_Business_Score)
+print("Introduction to Computer Systems :",Introduction_to_Computer_Systems_Score)
+print("Computer Programming :",Computer_Programming_Score)
